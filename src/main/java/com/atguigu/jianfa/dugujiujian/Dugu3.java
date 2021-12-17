fa2f812338f05369d39d7a932661a19f2a8addc9
liusonghuan <1245920053@qq.com>
2021-12-17 16:56:07 +0800

    岳不群修改

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu3.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu3.java
@@ -3,5 +3,10 @@ package com.atguigu.jianfa.dugujiujian;
 public class Dugu3 {
     public static void main(String[] args) {
         System.out.println("12345");
+        System.out.println("2222");
+        System.out.println("3333");
+        System.out.println("44444");
+        System.out.println("55555");
+
     }
 }
